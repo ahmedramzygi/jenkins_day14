ddb12d1c4c267dca9c0243fb14fb97bead650e9c
Ahmed Ramzy <aeramzy9@gmail.com>
2025-06-23 23:56:32 +0300

    adding files

---
+++ jenkins/script.groovy
@@ -0,0 +1,22 @@
+def buildJar() {
+    echo "building the application..."
+    sh 'mvn package'
+} 
+def testApp() {
+    echo "Testing the application..."
+
+} 
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t java-maven-app .'
+        sh "echo $PASSWORD | docker login -u $USERNAME $--password-stdin"
+        sh 'docker push docker push aeramzy9/java-maven-app:latest'
+    }
+} 
+
+def deployApp() {
+    echo 'deploying the application...'
+} 
+
+return this
